ff1b6d744acd4c80c42574fefdfe7020476f66e4
Pavel <pashaphb@gmail.com>
2016-07-02 18:40:45 +0300

    Hello 15

---
+++ helllooo/helllooo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helllooo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "heloo";
+            Console.Write(hello);
+            Console.ReadKey();
+
+        }
+    }
+}
